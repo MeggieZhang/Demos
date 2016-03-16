98dcd477cd02fc965e95437c22dc1aace90cb106
Mr_Wrong <Mr_Wrong@MacBook-Pro.local>
2016-03-16 10:55:10 +0800

    add asynchTaskFor70kg

--- app/src/main/java/com/demos/asynchronous/AsyncTaskActivity.java
+++ app/src/main/java/com/demos/asynchronous/AsyncTaskActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import com.demos.R;
 import com.demos.main.base.ToolBarActivity;
+import com.socks.library.KLog;
 
 /**
  * Created by Mr_Wrong on 16/2/22.
@@ -22,9 +23,9 @@ public class AsyncTaskActivity extends ToolBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         textView = mTool.find(R.id.tv_text);
-        AsyncTask task = new Task().execute(1);
+//        AsyncTask task = new Task().execute(1);
+        taskFor70kg();
     }
-
 
     class Task extends AsyncTask<Integer, Integer, Boolean> {
         int count = 0;
@@ -56,6 +57,34 @@ public class AsyncTaskActivity extends ToolBarActivity {
         @Override
         protected void onPostExecute(Boolean aBoolean) {
             textView.setText("执行结束了");
+        }
+    }
+
+    private void taskFor70kg() {
+        for (int i = 0; i < 6; i++) {
+            AsyncTaskFor70kg<String, Integer, String> taskFor70kg = new AsyncTaskFor70kg<String, Integer, String>() {
+                @Override
+                protected String doInBackground(String... params) {
+//                KLog.e("开始睡觉---" + params[0] + "--" + params[1]);
+                    KLog.e(Thread.currentThread().getName());
+                    for (int i = 0; i < 10; i++) {
+                        publishProgress(i);
+                    }
+                    return "睡完觉了";
+                }
+
+                @Override
+                protected void onProgressUpdate(Integer... values) {
+//                KLog.e("进度---" + values[0]);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+//                KLog.e("在主线程的结果---" + s);
+                    KLog.e("结束---" + Thread.currentThread().getName());
+                }
+            };
+            taskFor70kg.execute("arg1", "arg2");
         }
     }
 
--- app/src/main/java/com/demos/asynchronous/ThreadPool.java
+++ app/src/main/java/com/demos/asynchronous/ThreadPool.java
@@ -1,7 +1,13 @@
 package com.demos.asynchronous;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -26,19 +32,82 @@ public class ThreadPool {
             return new Thread(r, "70kg'Task #" + mCount.getAndIncrement());
         }
     };
-    public static final Executor executor
+    public static final Executor THREAD_POOL_EXECUTOR
             = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE,
             TimeUnit.SECONDS, sPoolWorkQueue, sThreadFactory);
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            executor.execute(new Runnable() {
+//        for (int i = 0; i < 10; i++) {
+//            executor.execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    System.out.println(Thread.currentThread().getName());
+//                }
+//            });
+//        }
+        ThreadPool pool = new ThreadPool();
+        pool.test();
+
+    }
+
+    private static volatile Executor defaultExecutor = new Executor() {
+        final ArrayDeque<Runnable> tasks = new ArrayDeque<Runnable>();
+        Runnable activeRunnable;
+
+        @Override
+        public void execute(final Runnable r) {
+            tasks.offer(new Runnable() {
+
                 @Override
                 public void run() {
-                    System.out.println(Thread.currentThread().getName());
+                    try {
+                        System.out.println(Thread.currentThread().getName());
+                        r.run();
+                    } finally {
+                        System.out.println("调度");
+                        scheduleNext();
+                    }
                 }
             });
+
+            if (activeRunnable == null) {
+                System.out.println("null");
+                scheduleNext();
+            }
         }
 
+        protected synchronized void scheduleNext() {
+            if ((activeRunnable = tasks.poll()) != null) {
+                THREAD_POOL_EXECUTOR.execute(activeRunnable);
+            }
+        }
+    };
+
+    private void test() {
+        List<FutureTask<String>> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            FutureTask<String> task = new FutureTask<String>(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("calling");
+                    Thread.sleep(1000);
+                    return "hello";
+                }
+            }) {
+                @Override
+                protected void done() {
+                    try {
+                        System.out.println("done " + get());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            list.add(task);
+        }
+        for (FutureTask<String> task : list)
+            defaultExecutor.execute(task);
     }
 }
